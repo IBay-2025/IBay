bc852b498c7d437ae58088e131a7a3e15aefceda
unclegrandpa979 <freestonetp@gmail.com>
2025-03-12 13:38:31 +0000

    tom

--- LoginPage/signUpPage.php
+++ LoginPage/signUpPage.php
@@ -5,6 +5,13 @@ $password = 'aey38pcb';
 $userName = 'cob295user';
 $dbName = 'cob295db';
 
+//
+$db = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
 
 
 ?>
