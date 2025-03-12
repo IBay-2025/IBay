a3042350eed9774766b8eab8669c96076f080d13
unclegrandpa979 <freestonetp@gmail.com>
2025-03-12 13:39:44 +0000

    Tom

--- LoginPage/signUpPage.php
+++ LoginPage/signUpPage.php
@@ -1,3 +1,16 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="robots" content="noindex, nofollow" />
+        <style type="text/css">
+        body{background-color:orange;font-size:1.1em;text-align:center;}
+        </style>
+        <title> Access MySQL </title>
+</head>
+<body>
+	<div style="background-color:yellow;width:20em;margin-left:auto;margin-right:auto">
+ 	<h2> Access MySQL Database with MySQLi</h2>
+	</div>
 <?php
 //set Variables
 $serverName = 'localhost';
@@ -12,6 +25,7 @@ $db = mysqli_connect($servername, $username, $password, $dbname);
 if (!$db) {
     die("Connection failed: " . mysqli_connect_error());
   }
+
 
 
 ?>
