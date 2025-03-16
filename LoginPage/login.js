96096637640ed44067054aa187b1c0345ca60ec6
BlessE1 <blessingoe16@gmail.com>
2025-03-16 13:31:19 +0000

    Added password validation

--- LoginPage/login.js
+++ LoginPage/login.js
@@ -1,4 +1,5 @@
-window.onscroll = function() {scrollFunction()};
+//Delete this if necessary//
+/*window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -6,20 +7,52 @@ function scrollFunction() {
   } else {
     document.getElementById("navbar").style.top = "-50px";
   }
-}
+}*/
 
+//Validation of password//
+var password = document.getElementById("password");
+var confirmPassword = document.getElementById("confirmPassword");
 
-
-
-function validatePassword(){
-  var password = document.getElementById("password");
-  var confirm_password = document.getElementById("confirm_password");
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
+//Event listener for password
+password.addEventListener("input", function () {
+  var passwordVal = document.getElementById("password").value;
+  //If password is "test", skip password validation
+  if (passwordVal == "test") {
+    password.setCustomValidity('');
   } else {
-    confirm_password.setCustomValidity('');
+    //Check if password is at least 8 characters
+    if (passwordVal.length < 8) {
+      password.setCustomValidity("Password must be at least 8 characters");
+    //Check if password contains at least one number
+    } else if (passwordVal.search(/[0-9]/) < 0) {
+      password.setCustomValidity("Password must contain at least one number");
+    //Check if password contains at least one uppercase letter
+    } else if (passwordVal.search(/[A-Z]/) < 0) {
+      password.setCustomValidity("Password must contain at least one uppercase letter");
+    //Check if password contains at least one lowercase letter
+    } else if (passwordVal.search(/[a-z]/) < 0) {
+      password.setCustomValidity("Password must contain at least one lowercase letter");
+    //Check if password contains at least one special character
+    } else if (passwordVal.search(/[^a-zA-Z0-9]/) < 0) {
+      password.setCustomValidity("Password must contain at least one special character");
+    } else {
+      password.setCustomValidity('');
+    }
+  }
+  password.reportValidity();
+})
+
+//Event listener for confirming password
+confirmPassword.addEventListener('input', function () {
+  var passwordVal = document.getElementById("password").value;
+  var confirmPasswordVal = document.getElementById("confirmPassword").value;
+
+  // Check if passwords match
+  if (passwordVal != confirmPasswordVal) {
+    confirmPassword.setCustomValidity("Passwords do not match");
+  } else {
+    confirmPassword.setCustomValidity('');
   }
 
-  password.onchange = validatePassword;
-  confirm_password.onkeyup = validatePassword;
-}
+  confirmPassword.reportValidity();
+});
