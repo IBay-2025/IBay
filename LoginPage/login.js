e01be6443adabc84687c8d4fda0cca9a4548f214
UncleGrandpa979 <freestonetp@gmail.com>
2025-04-25 12:01:11 +0100

    25/04/2025 morning

--- LoginPage/login.js
+++ LoginPage/login.js
@@ -1,78 +1,9 @@
-//VALIDATION FOR PASSWORD//
-var password = document.getElementById("password");
-var confirmPassword = document.getElementById("confirmPassword");
-
-//Event listener for password
-password.addEventListener("input", function () {
-  var passwordVal = document.getElementById("password").value;
-  //If password is "test", skip password validation
-  if (passwordVal == "test") {
-    password.setCustomValidity('');
-  } else {
-    //Check if password is at least 8 characters
-    if (passwordVal.length < 8) {
-      password.setCustomValidity("Password must be at least 8 characters");
-    //Check if password contains at least one number
-    } else if (passwordVal.search(/[0-9]/) < 0) {
-      password.setCustomValidity("Password must contain at least one number");
-    //Check if password contains at least one uppercase letter
-    } else if (passwordVal.search(/[A-Z]/) < 0) {
-      password.setCustomValidity("Password must contain at least one uppercase letter");
-    //Check if password contains at least one lowercase letter
-    } else if (passwordVal.search(/[a-z]/) < 0) {
-      password.setCustomValidity("Password must contain at least one lowercase letter");
-    //Check if password contains at least one special character
-    } else if (passwordVal.search(/[^a-zA-Z0-9]/) < 0) {
-      password.setCustomValidity("Password must contain at least one special character");
-    } else {
-      password.setCustomValidity('');
-    }
-  }
-  password.reportValidity();
-})
-
-//Event listener for confirming password
-confirmPassword.addEventListener('input', function () {
-  var passwordVal = document.getElementById("password").value;
-  var confirmPasswordVal = document.getElementById("confirmPassword").value;
-
-  // Check if passwords match
-  if (passwordVal != confirmPasswordVal) {
-    confirmPassword.setCustomValidity("Passwords do not match");
-  } else {
-    confirmPassword.setCustomValidity('');
-  }
-
-  confirmPassword.reportValidity();
-});
-
-//Event listener for validating postcode
-var postcode = document.getElementById("postcode");
-
-postcode.addEventListener('input', function () {
-  var postcodeVal = document.getElementById("postcode").value;
-  //Check if postcode is a valid UK postcode
-  if (!postcodeVal.match(/^[A-Z]{1,2}[0-9]{1,2}[A-Z]?\s?[0-9][A-Z]{2}$/)) {
-    postcode.setCustomValidity("Invalid UK postcode");
-  } else {
-    postcode.setCustomValidity('');
-  }
-
-  postcode.reportValidity();
-}) 
-
-//Event listener for validating telephone number
-var telephone = document.getElementById("phonenumber");
-
-telephone.addEventListener('input', function () {
-  var telephoneVal = document.getElementById("phonenumber").value;
-  //Check if telephone number is a valid UK telephone number
-  if (!telephoneVal.match(/^0[0-9]{10}$/)) {
-    telephone.setCustomValidity("Invalid UK telephone number");
-  } else {
-    telephone.setCustomValidity('');
-  }
-
-  telephone.reportValidity();
-})
-
+$(document).ready(function(){
+    $.get("login.php",function(data,status){
+        if(status == "success"){
+            alert("Login successful!");
+        }else{
+            alert("Login failed!");
+        }
+    });
+});	
--- SellerPage/sellerPage.php
+++ SellerPage/sellerPage.php
@@ -1,31 +1,5 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta name="robots" content="noindex, nofollow" />
-        <style type="text/css">
-        body{background-color:orange;font-size:1.1em;text-align:center;}
-        </style>
-        <title> Access MySQL </title>
-</head>
-<body>
-	<div style="background-color:yellow;width:20em;margin-left:auto;margin-right:auto">
- 	<h2> Access MySQL Database with MySQLi</h2>
-	</div>
 <?php
-//set Variables
-$serverName = 'localhost';
-$password = 'aey38pcb';
-$userName = 'cob295user';
-$dbName = 'cob295db';
-
-//
-$db = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check connection
-if (!$db) {
-    die("Connection failed: " . mysqli_connect_error());
-  }
-
+include '../connect.php';
 
 
 ?>
