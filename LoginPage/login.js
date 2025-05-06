963ca4aaa94b1f54f3582744f03ff0bc6fb61600
unclegrandpa979 <freestonetp@gmail.com>
2025-05-06 13:51:32 +0100

    change pop up message

--- LoginPage/login.js
+++ LoginPage/login.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $.get("login.php",function(data,status){
         if(status == "success"){
-            alert("Login successful!");
+            //alert("Login successful!");
         }else{
-            alert("Login failed!");
+            //alert("Login failed!");
         }
     });
 });	
