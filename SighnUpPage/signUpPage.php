a79958eb20970bec0f335dfb17b27b07824da417
unclegrandpa979 <freestonetp@gmail.com>
2025-05-06 13:46:24 +0100

    re organised files

--- SighnUpPage/signUpPage.php
+++ SighnUpPage/signUpPage.php
@@ -1,7 +1,7 @@
 <?php
 include '../connect.php';
 //echo('active debugging<br>');
-
+echo`Sign up page<br>`;
 if(isset($_POST['signUp-Submit'])){
     $firstName = $_POST['firstName'];
     $surname = $_POST['surname'];
@@ -19,21 +19,23 @@ if(isset($_POST['signUp-Submit'])){
 
     //$sql = "SELECT MAX(userId) FROM $membersTbl";
     //$result = mysqli_query($db, $sql);
-    echo('Query: ' . $result . '<br>');
-    $sql = "INSERT INTO $membersTbl (userId, password, name, email, address, postcode, rating, firstname, surname, gender, phone_number) 
-        VALUES (NULL, '$password', '$firstname + ' ' + $surname', '$email', '$address', '$postcode', 0, '$firstName', '$surname', '$gender', '$phoneNumber')";
+    
+    #$sql = "INSERT INTO $membersTbl (userId, password, firstname, email, address, postcode, rating, surname, gender, phone_number) 
+    #VALUES (NULL, '$password', '$firstName', '$email', '$address', '$postcode', 0, '$surname', '$gender', '$phoneNumber')";
+
     
     
-    $result = mysqli_query($db, $sql);
+    #$result = mysqli_query($db, $sql);
+    #echo('Query: ' . $result . '<br>');
     //use query results
-    if (!$result) {
-        die("Query failed: " . mysqli_error($db));
-    }
-    else{
+    #if (!$result) {
+     #   die("Query failed: " . mysqli_error($db));
+    #}
+    #else{
         echo('Sign up successful<br>');
-        header("Location: ../LoginPage/login.php");
+        header("Location: ../LoginPage/lohin.html");
         exit();
-    }
+    #   }
 
 }
 else{
