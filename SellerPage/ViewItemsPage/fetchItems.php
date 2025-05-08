b86e47f08f7c006dfc1ca1ea51ee5dab0c64b653
unclegrandpa979 <freestonetp@gmail.com>
2025-05-08 14:04:35 +0100

    addItem.php

---
+++ SellerPage/ViewItemsPage/fetchItems.php
@@ -0,0 +1,24 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);   
+include '../../connect.php'; 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!isset($_SESSION['userId'])) {
+        die("User not logged in.");
+    }
+    $userId = $_SESSION['userId'];
+
+    $query = "SELECT * FROM iBayItems WHERE userId = $userId";
+    $result = mysqli_query($db, $query);
+    if (!$result) {
+        die("Query failed: " . mysqli_error($db));
+    }
+    
+
+
+
+
+
+    echo json_encode($items);
+}
+?>
