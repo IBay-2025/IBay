743a236c03a4d42fd17e42d58e2bfbfe5c71b088
BlessE1 <blessingoe16@gmail.com>
2025-05-12 12:34:59 +0100

    some js for basket

--- BuyersPage/basket/basket.js
+++ BuyersPage/basket/basket.js
@@ -1,12 +1,34 @@
 //jQuery function for deleting an item from the basket
 $(document).on("click", ".remove-btn", function() {
-    alert("s"); 
-    alert("s"); 
+    //Remove the row of the button that was clicked
     $(this).closest("tr").remove();
+
+    //Subtract the price and quantity of the item from the subtotal
+    var subtotal = parseFloat($("#subtotal").text().split("£")[1]);
+    var totalPrice = parseFloat($(this).closest("tr").find(".totalPrice").text());
+    $("#subtotal").html(`<strong>Subtotal:</strong> £${(subtotal - totalPrice).toFixed(2)}`);
 });
 
-$(document).on("click", ".checkout-btn", function() {
-    alert("s"); 
-    alert("s"); 
-    $(this).closest("tr").remove();
+//jQuery function for calcating the total price for each item with respect to quantity in the basket
+$(document).ready(function() {
+    var rows = $("tbody").find("tr");
+    $.each(rows, function() {
+        var price = parseFloat($(this).find(".price").text());
+        var quantity = parseFloat($(this).find(".quantity").text());
+        var total = price * quantity;
+        $(this).find(".totalPrice").html(total);
+    });
+});
+
+
+//jQuery function for summing up the total price (subtotal) of all items in the basket
+$(document).ready(function() {
+    var subtotal = 0;
+    var totalPrices = $(document).find(".totalPrice");
+    $.each(totalPrices, function() {
+        subtotal += parseFloat($(this).text());
+    })
+
+    //Round the subtotal to two decimal places and display it in the subtotal element with the text
+    $("#subtotal").html(`<strong>Subtotal:</strong> £${subtotal.toFixed(2)}`); //Round the subtotal to two decimal places and display it in the subtotal element with the text
 })
